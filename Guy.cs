82c9f970e062ead07eee19d635587d39086b4384
JSpanjaard07 <joeyamgelo123@outlook.com>
2026-07-03 10:35:12 +0200

    Initial Commit
    Volgensmij een heel eind klaar zo, werkt alleen nog niet omdat ik Bet nog niet heb gemaakt

--- Guy.cs
+++ Guy.cs
@@ -8,5 +8,35 @@ namespace DogsAtTheRaces
 {
     internal class Guy
     {
+        public string Name;
+        public Bet MyBet;
+        public int Cash;
+
+        public bool PlaceBet(int amount, int dog)
+        {
+            if (amount > Cash)
+            {
+                return: false;
+            }
+
+            MyBet = new Bet
+            {
+                Amount = amount,
+                Dog = dog,
+                Better = this
+            };
+
+            return true;
+        }
+
+        public void Collect(int winner)
+        {
+            if (MyBet != null)
+            {
+                Cash += MyBet.PayOut(winner);
+                MyBet = null;
+            }
+        }
+
     }
 }
